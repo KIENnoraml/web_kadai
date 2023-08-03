5c352fc6e693ca7fc12030d12fa2a0649e711f43
KIENnoraml <104120139+KIENnoraml@users.noreply.github.com>
2023-08-03 10:33:26 +0900

    Initial commit

---
+++ js/kadai5js.js
@@ -0,0 +1,13 @@
+
+
+function viet(){
+
+    $('.slider').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dot: true,
+        slidesToShow:5,
+        slidesToScroll:1,
+    });
+}
+
